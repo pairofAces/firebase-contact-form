b5c5027a15ea38e97be4899193d8f324d7a6e018
pairofAces <karanchauhan16@gmail.com>
2020-12-14 20:42:05 -0500

    added inputs to contact form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,8 +2,22 @@ import React from 'react'
 
 function Contact() {
     return (
-        <form>
-            
+        <form className="form">
+            <h1>Contact Form</h1>
+            <label>
+                Name
+                <input  placeholder="name"/>
+            </label>
+
+            <label>
+                Email
+                <input  placeholder="email"/>
+            </label>
+
+            <label>
+                Message
+                <textarea placeholder="message"></textarea>
+            </label>
         </form>
     )
 }
